0b29694a2dc570d1e9ac597500efb79726636b47
Dalfonzo <29Dalfonzo@gmail.com>
2022-03-18 15:10:15 -0400

    Initial commit with Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/Header.css'
+const Header= () => {
+	return (
+	<div className='header'>
+		<span className='title'> 
+			HACKER NEWS 
+			</span>
+	</div>
+	)
+}
+
+export default Header
